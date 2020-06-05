2b4022753e51f4040e6087beeefd2020f62777d8
ddamjengi1020 <silluat11@gmail.com>
2020-06-05 14:18:57 +0900

    MSEdge adapted

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,17 +2,12 @@ import { resultCases } from "./resultCases";
 
 const selectBtn = document.querySelectorAll(".button");
 const first = document.querySelector(".slide:nth-child(1)");
+const last = document.querySelector(".slide:nth-child(14)");
 const loadedMain = document.querySelector("main");
 const loadingAni = document.getElementById("jsResorceLoading");
 
 const selectNums = [];
-const nextSlideEvent = (
-  duration = 0,
-  opacityDuration = 0,
-  isSlide = false,
-  vertical = false,
-  event = null
-) => {
+const nextSlideEvent = (duration = 0, event = null) => {
   if (event) {
     const { id: parent } = event.target.parentNode.dataset;
     const { id: child } = event.target.dataset;
@@ -23,7 +18,6 @@ const nextSlideEvent = (
     }
   }
   // 매 함수 호출시마다 브라우저 width 길이를 업뎃함 -> 애니메이션 변수로 사용하기떄문
-  const WINDOW = vertical ? window.innerHeight : window.innerWidth;
   const PREV = "prev";
   const ACTIVE = "active";
   const NEXT = "next";
@@ -35,62 +29,28 @@ const nextSlideEvent = (
     first.style.zIndex = 1;
     first.style.opacity = 1;
     first.style.pointerEvents = "initial";
-    first.nextElementSibling.id = NEXT;
+    last.id = PREV;
+    first.nextSibling.id = NEXT;
   } else {
     if (prevEl) {
       prevEl.removeAttribute("id");
     }
     activeEl.id = PREV;
     nextEl.id = ACTIVE;
-    first.removeAttribute("style");
-    activeEl.style.pointerEvents = "none";
-    const loadingScreen = nextEl.className.includes("loading");
-    const resultScreen = nextEl.className.includes("result");
-    activeEl.animate(
-      // 실행 시점에선 nextEl은 Prev 상태의 Element를 가리킴
-      {
-        transform: resultScreen ? [] : isSlide ? (vertical ? [] : []) : [],
-        zIndex: [1, 1],
-        opacity: [1, 0],
-      },
-      { duration: duration ? duration - 200 : 0, fill: "forwards" }
-    );
-    nextEl.animate(
-      // 실행 시점에선 nextEl은 현재 Active 상태의 Element를 가리킴
-      {
-        transform: resultScreen
-          ? []
-          : isSlide
-          ? vertical
-            ? [`translateY(-${WINDOW}px)`, `translateY(0)`]
-            : [`translateX(${WINDOW}px)`, `translateX(0)`]
-          : [],
-        zIndex: [1, 2],
-        opacity: [0, 1],
-      },
-      {
-        duration: duration ? duration : 0,
-        fill: "forwards",
-        delay: resultScreen
-          ? opacityDuration
-          : loadingScreen
-          ? opacityDuration + 300
-          : vertical
-          ? opacityDuration - 300
-          : opacityDuration - 200,
-      }
-    );
-    // select 버튼 이벤트 nesting 오류 fix
-    setTimeout(
-      () => (nextEl.style.pointerEvents = "initial"),
-      opacityDuration + 500
-    );
-
-    if (!nextEl.nextElementSibling) {
+    if (!nextEl.nextSibling) {
       first.id = NEXT;
     } else {
-      nextEl.nextElementSibling.id = NEXT;
+      nextEl.nextSibling.id = NEXT;
     }
+
+    first.removeAttribute("style");
+    activeEl.removeAttribute("style");
+    const loadingScreen = nextEl.className.includes("loading");
+
+    // select 버튼 이벤트 nesting 오류 fix
+    setTimeout(() => {
+      nextEl.style.pointerEvents = "initial";
+    }, duration + 500);
     // loading finish animation
     if (loadingScreen) {
       const loadingEl = document.getElementById("jsLoadingAnimation");
@@ -105,25 +65,24 @@ const nextSlideEvent = (
     if (selectNums.length === 12) {
       setTimeout(() => {
         resultCases(selectNums);
-      }, opacityDuration);
+      }, duration);
     }
   }
-};
-
-let lastTouchEnd = 0;
-
-const touchEndPrevent = (event) => {
-  const now = new Date().getTime();
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault();
-  }
-  lastTouchEnd = now;
 };
 
 const touchStartPrevent = (event) => {
   if (event.touches.length > 1) {
     event.preventDefault();
   }
+};
+
+let lastTouchEnd = 0;
+const touchEndPrevent = (event) => {
+  const now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
 };
 
 const init = () => {
@@ -142,12 +101,9 @@ const init = () => {
   });
 
   nextSlideEvent();
-
   selectBtn.forEach(
     (button) =>
-      button.addEventListener("click", (event) =>
-        nextSlideEvent(600, 800, true, false, event)
-      ) // 애니메이션 속도, 딜레이시간, 슬라이드 온오프(기본 fasle)
+      button.addEventListener("click", (event) => nextSlideEvent(600, event)) // 애니메이션 속도, 클릭이벤트
   );
 };
 
