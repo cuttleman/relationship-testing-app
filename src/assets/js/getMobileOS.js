9dba83b1fa456565c989fc3003676a98cc8c018d
ddamjengi1020 <silluat11@gmail.com>
2020-06-15 10:40:48 +0900

    start image size change

--- src/assets/js/getMobileOS.js
+++ src/assets/js/getMobileOS.js
@@ -2,13 +2,13 @@ export const getMobileOS = () => {
   const varUA = navigator.userAgent.toLowerCase();
   const smsLink = document.getElementById("sms-link-btn");
   if (varUA.indexOf("android") > -1) {
-    smsLink.href = "sms:?body=https://cocky-yalow-588613.netlify.app/";
+    smsLink.href = "sms:?body=https://jeokdanghi-relationship.com/";
   } else if (
     varUA.indexOf("iphone") > -1 ||
     varUA.indexOf("ipad") > -1 ||
     varUA.indexOf("ipod") > -1
   ) {
-    smsLink.href = "sms:&body=https://cocky-yalow-588613.netlify.app/";
+    smsLink.href = "sms:&body=https://jeokdanghi-relationship.com/";
   } else {
     return;
   }
