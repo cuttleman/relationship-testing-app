e5556df57706611e875946a901deaacb8192c2fe
ddamjengi1020 <silluat11@gmail.com>
2020-06-21 04:10:30 +0900

    fuck start page image shit

--- src/assets/js/resourceLoaded.js
+++ src/assets/js/resourceLoaded.js
@@ -3,19 +3,13 @@ const loadedMain = document.querySelector("main");
 const loadingAni = document.getElementById("jsResorceLoading");
 
 const loadHandler = () => {
-  let imageLoaded = false;
   for (let i = 0; i < images.length; i++) {
     images[i].src = images[i].dataset.src;
-    if (i === images.length - 1) {
-      imageLoaded = true;
-    }
   }
-  if (imageLoaded) {
-    setTimeout(() => {
-      loadedMain.style.display = "flex";
-      loadingAni.style.display = "none";
-    }, 300);
-  }
+  images[images.length - 1].addEventListener("load", () => {
+    loadedMain.style.display = "flex";
+    loadingAni.style.display = "none";
+  });
 };
 
 export const resourceLoaded = () =>
